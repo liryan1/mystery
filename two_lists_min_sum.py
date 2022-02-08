fc2689b4efed8a12921166fa661e879566fa0afc
Ryan Li <ryan.li@yale.edu>
2022-02-08 00:54:02 -0500

    Update two_lists_min_sum.py

--- two_lists_min_sum.py
+++ two_lists_min_sum.py
@@ -1,4 +1,4 @@
-def partition_cost(A, s1, s2, i, memo=None):
+def partition_cost(A: list, s1: int, s2: int, i: int, memo=None) -> int:
     '''Returns the minimum difference of partitioning list A into two.
     No constraint on size of each list
     '''
@@ -15,24 +15,25 @@ def partition_cost(A, s1, s2, i, memo=None):
     return memo[key]
 
 
-def partition(A):
+def partition(A: list) -> tuple(int, list):
     ''' Partition list A into two sublists where the difference of sums are minimum.
+    Returns tuple of int: minimum difference of sums & list of elements in first list.
     '''
     memo = {}
-    def dp(A, s1, s2, i, L1, L2):
+    def dp(A, s1, s2, i, choice):
         if i >= len(A):
-            return (abs(s1 - s2), L1, L2)
+            return (abs(s1 - s2), choice)
 
         key = (i, s1)
         if key not in memo:
-            case1, L11, L12 = dp(A, s1 + A[i], s2, i+1, L1+[A[i]], L2)
-            case2, L21, L22 = dp(A, s1, s2 + A[i], i+1, L1, L2 + [A[i]])
+            case1, choice1 = dp(A, s1 + A[i], s2, i+1, choice + [A[i]])
+            case2, choice2 = dp(A, s1, s2 + A[i], i+1, choice)
             if case1 <= case2:
-                memo[key] = (case1, L11, L12)
+                memo[key] = (case1, choice1)
             else:
-                memo[key] = (case2, L21, L22)
+                memo[key] = (case2, choice2)
         return memo[key]
-    return dp(A, 0, 0, 0, [], [])
+    return dp(A, 0, 0, 0, [])
 
 if __name__ == "__main__":
     A = [5, 7, 2, 1, 10, 6, 8]
