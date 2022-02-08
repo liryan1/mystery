afaa95340e3e205b2803b750ff1c6a50c88abebc
Ryan Li <ryan.li@yale.edu>
2022-02-08 01:34:02 -0500

    Update two_lists_min_sum.py

--- two_lists_min_sum.py
+++ two_lists_min_sum.py
@@ -1,21 +1,24 @@
-def partition_cost(A: list, s1: int, s2: int, i: int, memo=None) -> int:
+import numpy as np
+
+def partition_cost(A: list) -> int:
     '''Returns the minimum difference of partitioning list A into two.
     No constraint on size of each list
     '''
-    if memo is None: memo = {}
-    if i >= len(A):
-        return abs(s1 - s2)
+    memo = {}
+    def dp(A: list, s1: int, s2: int, i: int):
+        if i >= len(A):
+            return abs(s1 - s2)
 
-    key = (i, s1)
-    if key not in memo:
-        case1 = partition_cost(A, s1 + A[i], s2, i+1)
-        case2 = partition_cost(A, s1, s2 + A[i], i+1)
-        memo[key] = min(case1, case2)
+        key = (i, s1)
+        if key not in memo:
+            case1 = dp(A, s1 + A[i], s2, i+1)
+            case2 = dp(A, s1, s2 + A[i], i+1)
+            memo[key] = min(case1, case2)
+        return memo[key]
+    return dp(A, 0, 0, 0)
 
-    return memo[key]
 
-
-def partition(A: list) -> tuple(int, list):
+def partition(A: list) -> tuple[int, list]:
     ''' Partition list A into two sublists where the difference of sums are minimum.
     Returns tuple of int: minimum difference of sums & list of elements in first list.
     '''
@@ -35,6 +38,25 @@ def partition(A: list) -> tuple(int, list):
         return memo[key]
     return dp(A, 0, 0, 0, [])
 
+
+def partition_cost_bu(A:list) -> int:
+    ''' Bottom up approach of partition_cost.
+    '''
+    m = sum(A)
+    table = np.zeros((m, len(A)))
+    table[A[0]-1, 0] = 1
+    for i in range(1, len(A)):
+        for j in range(m):
+            if table[j, i-1]:
+                table[j, i] = 1
+                table[j+A[i]-1, i] = 1
+    for j in range(m//2):
+        if table[m//2+j, -1] or table[m//2-j, -1]:
+            return j + m % 2
+    return -1
+
+
 if __name__ == "__main__":
     A = [5, 7, 2, 1, 10, 6, 8]
-    print(partition(A))
+    print(partition_cost(A))
+    print(partition_cost_bu(A))
