62312ab1f05804913dc225844bde722458b1a13e
Ryan Li <ryan.li@yale.edu>
2022-02-26 12:36:31 -0500

    fixed bug

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,13 @@
 def searchRange(nums: list[int], target: int) -> list[int]:
     beg, end = 0, len(nums) - 1
     # find the left index
-    while beg < end:
+    while beg <= end:
         mid = (beg + end) // 2
         if nums[mid] == target:
             if (mid - 1 < 0) or nums[mid - 1] < target:
                 start = mid
                 break
-            end = mid
+            end = mid - 1
         elif nums[mid] < target:
             beg = mid + 1
         elif nums[mid] > target:
@@ -15,9 +15,10 @@ def searchRange(nums: list[int], target: int) -> list[int]:
     else:
         start = -1
 
+
     # find the right index
     beg, end = 0, len(nums) - 1
-    while beg < end:
+    while beg <= end:
         mid = (beg + end) // 2
         if nums[mid] == target:
             if (mid + 1 >= len(nums)) or nums[mid + 1] > target:
