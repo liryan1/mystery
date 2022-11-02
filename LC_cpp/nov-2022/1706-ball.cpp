846356fe8ddc039eaa6131b157ab32ed134c78a8
Ryan Li <ryan.li@ny-go.org>
2022-11-01 22:39:03 -0400

    nov1-2

---
+++ LC_cpp/nov-2022/1706-ball.cpp
@@ -0,0 +1,39 @@
+// If current value is -1, don't do anything
+// If grid[i][j] == 1 (pointing right), if j+1 is out of bounds or pointing left, set current value to -1
+// Similar & opposite logic with == -1
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int m = grid.size(), n  = grid[0].size();
+        vector<int> land(n, 0);
+        for (int i = 0; i < n; ++i) {
+            land[i] = i;
+        }
+        for (int j = 0; j < m; ++j) {
+            // Check each ball at current level and update it
+            for (int i = 0; i < n; ++i) {
+                int curr_pos = land[i];
+                if (curr_pos == -1) continue;
+                if (grid[j][curr_pos] == 1) {
+                    if (curr_pos == n - 1 || grid[j][curr_pos + 1] == -1) {
+                        land[i] = -1;
+                    } else {
+                        land[i]++;
+                    }
+                } else {
+                    // grid == -1
+                    if (curr_pos == 0 || grid[j][curr_pos - 1] == 1) {
+                        land[i] = -1;
+                    } else {
+                        land[i]--;
+                    }
+                }
+            }
+        }
+        return land;
+    }
+};
---
+++ LC_cpp/nov-2022/433-min_genetic_mutation.cpp
@@ -0,0 +1,55 @@
+#include<string>
+#include<vector>
+#include<unordered_set>
+#include<queue>
+using namespace std;
+
+// https://leetcode.com/problems/minimum-genetic-mutation/
+class Solution {
+private:
+    // check possible changes by replacing current letter with ACGT
+    void updateQ(queue<string> &q, string &curr, unordered_set<string> &set) {
+        for (int i = 0; i < curr.length(); ++i) {
+            string temp = curr;
+            temp[i] = 'A';
+            if (set.count(temp))
+                q.push(temp);
+            temp[i] = 'C';
+            if (set.count(temp))
+                q.push(temp);
+            temp[i] = 'G';
+            if (set.count(temp))
+                q.push(temp);
+            temp[i] = 'T';
+            if (set.count(temp))
+                q.push(temp);
+        }
+    }
+
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set<string> set{bank.begin(), bank.end()};
+        queue<string> Q;
+        string curr;
+        int steps = 0, iter = 0;
+        
+        if (!set.count(end))
+            return -1;
+
+        Q.push(start);
+        while (!Q.empty()) {
+            iter = Q.size();
+            while (iter--) {
+                curr = Q.front();
+                Q.pop();
+                if (curr == end) // found the answer
+                    return steps;
+
+                set.erase(curr); // erase the current to avoid redundant checking
+                updateQ(Q, curr, set);
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
